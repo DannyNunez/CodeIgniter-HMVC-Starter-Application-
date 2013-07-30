cab301b994ea24f79601bade3629426da7af54a6
BigGuns99 <danny.nunez15@gmail.com>
2013-07-30 07:10:20 -0700

    added $data array to MY_Controller Added Bcrypt.php and Ion_Auth to the application/libraries directories

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,11 +1,14 @@
-<?php
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
 
 /**
  * Description of MY_Controller
  * @author dnunez
  */
+
 class MY_Controller extends MX_Controller
 {
+
+    public $data = array();
 
     function __construct()
     {
